1e1f40d8dba8cbbe8580399eb66fdbf34b52de84
marcinowski <marcinowski007@gmail.com>
2018-02-21 02:10:48 +1100

    Add username to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217120125) do
+ActiveRecord::Schema.define(version: 20180220135024) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -46,7 +46,9 @@ ActiveRecord::Schema.define(version: 20180217120125) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
