e8c2517909f96aa79e55805b0ab2ec45cc19033b
marcinowski <marcinowski007@gmail.com>
2018-02-22 22:21:28 +1100

    Fix user page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :subs, param: :slug, path: :r, only: [:show, :new, :create, :destroy] do
     resources :posts, only: [:new]
   end
-  resources :posts, only: [:index, :show, :edit, :update, :destroy] do
+  resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: [:index, :new, :create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
