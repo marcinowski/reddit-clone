d598046d90f555ca5ef214e56d734668d21884b2
marcinowski <marcinowski007@gmail.com>
2018-02-22 22:00:10 +1100

    Proper new_user_path POST/GET routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users, param: :username, path: :u
+  get '/u/new', to: 'users#new', as: :new_user
+  post '/u/new', to: 'users#create', as: nil
+  resources :users, param: :username, path: :u, only: [:show]
   resources :subs, param: :slug, path: :r, only: [:show, :new, :create, :destroy] do
     resources :posts, only: [:new]
   end
-  resources :posts, only: [:index, :show, :edit, :update, :destroy, :new, :create] do
+  resources :posts, only: [:index, :show, :edit, :update, :destroy] do
     resources :comments, only: [:index, :new, :create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
