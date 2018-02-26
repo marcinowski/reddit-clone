3273568ca21f9ac2f0a25fa340c94e3f09b42dd7
marcinowski <marcinowski007@gmail.com>
2018-02-27 02:11:17 +1100

    Upvotes: front p2 jquery

--- app/assets/javascripts/custom.coffee
+++ app/assets/javascripts/custom.coffee
@@ -1,16 +1,33 @@
-@vote = (model, id, dir, element) ->
-  $.post '/ratings/',
-    'model': model,
-    'id': id,
-    'dir': dir
-    (data) -> null # -> element
+@vote = (model, id, dir, element, sibling) ->
+  $.post
+    url: '/ratings/',
+    type: 'POST',
+    headers: {
+      'X-Transaction': 'POST Example',
+      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+    },
+    data: {
+      'model': model,
+      'id': id,
+      'dir': dir
+    },
+    error: (jqXHR, textStatus, errorThrown) ->
+      console.log(textStatus);
+    success: (data, textStatus, jqXHR) ->
+      $(element).toggleClass('rating-highlight')
+      $(sibling).removeClass('rating-highlight')
+      score = $(element).siblings('.rating-score')
+      s = parseInt(score.text());
+      score.text(s+dir);
 
 $ ->
   $(".rating-downvote").click (e) ->
     model = $(this).data("model")
     id = $(this).data("id")
-    vote(model, id, -1, this)
+    sibling = $(this).siblings('.rating-upvote')
+    vote(model, id, -1, this, sibling)
   $(".rating-upvote").click (e) ->
     model = $(this).data("model")
     id = $(this).data("id")
-    vote(model, id, 1, this)
+    sibling = $(this).siblings('.rating-downvote')
+    vote(model, id, 1, this, sibling)
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,28 +1,33 @@
 class RatingsController < ApplicationController
   def vote
+    if current_user.nil?
+      flash[:danger] = "Log in mate"
+      return
+    end
     model = params[:model]
-    id = params[:id]
+    id = params[:id].to_i
     if model.nil? || id.nil?
       head 500
       return
     end
-    if params[:dir] == 0
+    dir = params[:dir].to_i
+    if dir == 0
       dir = 0
     else
-      dir = params[:dir] > 0 ? 1 : -1
+      dir = dir > 0 ? 1 : -1
     end
     if model == 'post'
-      object = RatingPost.find_by(user_id: @current_user.id, post_id: id)
+      object = RatingPost.find_by(user_id: current_user.id, post_id: id)
       if object.nil?
-        RatingPost.create(user_id: @current_user.id, post_id: id, rating: dir)
+        RatingPost.create(user_id: current_user.id, post_id: id, rating: dir)
       else
         object.rating = dir
         object.save
       end
     elsif model == 'comment'
-      object = RatingComment.find_by(user_id: @current_user.id, post_id: id)
+      object = RatingComment.find_by(user_id: current_user.id, post_id: id)
       if object.nil?
-        RatingComment.create(user_id: @current_user.id, post_id: id, rating: dir)
+        RatingComment.create(user_id: current_user.id, post_id: id, rating: dir)
       else
         object.rating = dir
         object.save
