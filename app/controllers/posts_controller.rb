6a51a165c8166ba819244138f7bee6c476af3a9b
marcinowski <marcinowski007@gmail.com>
2018-03-15 22:40:45 +1100

    Refactor posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,42 +1,47 @@
 class PostsController < ApplicationController
   include PostsHelper
+
   def new
-    @title = 'Create a Post'
-    if !logged_in?
+    unless logged_in?
       flash[:danger] = "You must be logged in to add posts."
-      redirect_to root_url
-      return
+      return head(:unauthorized)
     end
     sub = Sub.find_by(slug: params[:sub_slug])
     if sub.nil?
-      @post = Post.new
+      post = Post.new
     else
       unless UserPermissions.can_post?(current_user, sub)
+        logger.debug "User #{current_user.id} cannot add posts to sub #{sub.id}"
         flash[:danger] = "You can't add posts."
-        redirect_to root_url
-        return
+        return head(:forbidden)
       end
-      @post = Post.new(sub_id: sub.id)
+      post = Post.new(sub_id: sub.id)
     end
+    @title = 'Create a Post'
+    @post = post
   end
 
   def create
-    if !logged_in?
+    unless logged_in?
       flash[:danger] = "You must be logged in to add posts."
-      redirect_to root_url
-      return
+      return redirect_to root_url
     end
-    sub = Sub.find(post_params[:sub_id])
+    begin
+      sub = Sub.find(post_params[:sub_id])
+    rescue ActiveRecord::RecordNotFound
+      return head(:not_found)
+    end
     unless UserPermissions.can_post?(current_user, sub)
-      redirect_to root_url
-      return
+      return redirect_to root_url
     end
-    @post = Post.new(user_id: current_user.id, title: post_params[:title],
-      url: post_params[:url], description: post_params[:description], sub: sub)
-    if @post.save
-      redirect_to post_comments_path(post_id: @post.id)
+    post = sub.posts.new(user_id: current_user.id, title: post_params[:title],
+      url: post_params[:url], description: post_params[:description])
+    if post.save
+      return redirect_to post_comments_path(post)
     else
-      render "new"
+      logger.error post.errors.full_messages.join('\n')
+      @post = post
+      return render "new"
     end
   end
 
@@ -49,46 +54,61 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @title = 'Edit Post'
-    if logged_in?
-      @post = Post.find_by(id: params[:id], user: current_user)
-      if @post.nil?
-        redirect_to root_path
-      end
-    else
-      redirect_to root_path
+    unless logged_in?
+      return head(:unauthorized)
     end
+    begin
+      post = Post.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.debug "Post #{params[:id]} not found"
+      return head(:not_found)
+    end
+    unless post.user_id === current_user.id
+      return head(:forbidden)
+    end
+    @post = post
+    @title = 'Edit Post'
   end
 
   def update
-    if logged_in?
-      @post = Post.find_by(id: params[:id], user: current_user)
-      if @post.nil?
-        redirect_to root_path
-      else
-        if @post.update(post_params)
-          redirect_to post_comments_path(@post)
-        else
-          render "edit"
-        end
-      end
+    unless logged_in?
+      return head(:unauthorized)
+    end
+    begin
+      post = Post.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.debug "Post #{params[:id]} not found"
+      return head(:not_found)
+    end
+    unless post.user_id === current_user.id
+      return head(:forbidden)
+    end
+    if post.update(post_params)
+      return redirect_to post_comments_path(post)
     else
-      redirect_to root_path
+      @post = post
+      return render "edit"
     end
   end
 
   def destroy
-    if !logged_in?
-      flash[:warning] = "You must be logged in to edit the posts"
-      redirect_to root_path
+    unless logged_in?
+      return head(:unauthorized)
+    end
+    begin
+      post = Post.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.debug "Post #{params[:id]} not found"
+      return head(:not_found)
+    end
+    unless post.user_id === current_user.id
+      return head(:forbidden)
+    end
+    if post.delete
+      return redirect_to root_path
     else
-      post = Post.find_by(id: params[:id], user: current_user)
-      if !post.nil?
-        post.delete
-      else
-        flash[:danger] = "It's not your post dude"
-      end
-      redirect_to root_path
+      logger.error "posts/destroy Couldn't delete post #{post.id}"
+      return head(:internal_server_error)
     end
   end
 
--- test/integration/posts_test.rb
+++ test/integration/posts_test.rb
@@ -9,19 +9,19 @@ class PostsTest < ActionDispatch::IntegrationTest
 
   test "get add post path not authenticated" do
     get new_sub_post_path(sub_slug: subs(:one).slug)
-    assert_response :redirect
+    assert_response 401
   end
 
   test "get add post path banned" do
     log_in_as(users(:ban))
     get new_sub_post_path(sub_slug: subs(:one).slug)
-    assert_response :redirect
+    assert_response 403
   end
 
   test "get add post path banned on sub" do
     log_in_as(users(:ban_sub))
     get new_sub_post_path(sub_slug: subs(:one).slug)
-    assert_response :redirect
+    assert_response 403
   end
 
   test "post create post authenticated" do
@@ -29,14 +29,12 @@ class PostsTest < ActionDispatch::IntegrationTest
     assert_difference('Post.count') do
       post posts_path, params: {post: {sub_id: subs(:one).id, title: 'Test', description: 'test'}}
     end
-    assert_redirected_to post_comments_path(post_id: Post.last.id)
   end
 
   test "post create post not authenticated" do
     assert_no_difference('Post.count') do
       post posts_path, params: {post: {sub_id: subs(:one).id, title: 'Test', description: 'test'}}
     end
-    assert_redirected_to root_path
   end
 
   test "post create post banned" do
@@ -55,43 +53,42 @@ class PostsTest < ActionDispatch::IntegrationTest
 
   test "post edit post path not authenticated" do
     get edit_post_path(id: posts(:one).id)
-    assert_redirected_to root_path
+    assert_response 401
   end
 
   test "post edit post path not owner" do
     log_in_as(users(:two))
     get edit_post_path(id: posts(:one).id)
-    assert_redirected_to root_path
+    assert_response 403
   end
 
   test "post edit post path owner" do
     log_in_as(users(:one))
     get edit_post_path(posts(:one))
-    assert_response 200
+    assert_response :success
   end
 
   test "put update post path not authenticated" do
     put post_path(posts(:one))
-    assert_redirected_to root_path
+    assert_response 401
   end
 
   test "put update post path not owner" do
     log_in_as(users(:two))
     put post_path(posts(:one))
-    assert_redirected_to root_path
+    assert_response 403
   end
 
   test "put update post path owner" do
     log_in_as(users(:one))
     put post_path(posts(:one)), params: {post: {title: 'Test changed'}}
-    assert_redirected_to post_comments_path(posts(:one))
+    assert_response :redirect
   end
 
   test "delete post path not authenticated" do
     assert_no_difference('Post.count') do
       delete post_path(id: posts(:one).id)
     end
-    assert_redirected_to root_path
   end
 
   test "delete post path not owner" do
@@ -99,7 +96,6 @@ class PostsTest < ActionDispatch::IntegrationTest
     assert_no_difference('Post.count') do
       delete post_path(id: posts(:one).id)
     end
-    assert_redirected_to root_path
   end
 
   test "delete post path owner" do
@@ -107,6 +103,5 @@ class PostsTest < ActionDispatch::IntegrationTest
     assert_difference('Post.count', -1) do
       delete post_path(id: posts(:one).id)
     end
-    assert_redirected_to root_path
   end
 end
